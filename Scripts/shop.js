b0ea575b4cc930c102fa3575176fec8d09395c23
kouete kenneth <kouete678@gmail.com>
2023-08-21 08:18:22 +0100

    home page, shopping page, shopping cart developped

---
+++ Scripts/shop.js
@@ -0,0 +1,559 @@
+// Define products data
+// const productsData = [
+//     {
+//       id: 1,
+//       name: 'Product 1',
+//       category: 'watches',
+//       price: 10.99,
+//       image: '../images/product-image1.jpg',
+//       details:'',
+//     },
+//     {
+//         id: 1,
+//         name: 'Product 1',
+//         category: 'watches',
+//         price: 10.99,
+//         image: '../images/product-image1.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 1,
+//         name: 'Product 1',
+//         category: 'watches',
+//         price: 10.99,
+//         image: '../images/product-image1.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 1,
+//         name: 'Product 1',
+//         category: 'watches',
+//         price: 10.99,
+//         image: '../images/product-image1.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 1,
+//         name: 'Product 1',
+//         category: 'watches',
+//         price: 10.99,
+//         image: '../images/product-image1.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 1,
+//         name: 'Product 1',
+//         category: 'watches',
+//         price: 10.99,
+//         image: '../images/product-image1.jpg',
+//         details:'',
+//     },
+//     {
+//       id: 2,
+//       name: 'Product 2',
+//       category: 'shoes',
+//       price: 20.99,
+//       image: '../images/product-image2.jpg',
+//       details:'',
+//     },
+//     {
+//         id: 2,
+//         name: 'Product 2',
+//         category: 'shoes',
+//         price: 20.99,
+//         image: '../images/product-image2.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 2,
+//         name: 'Product 2',
+//         category: 'shoes',
+//         price: 20.99,
+//         image: '../images/product-image2.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 2,
+//         name: 'Product 2',
+//         category: 'shoes',
+//         price: 20.99,
+//         image: '../images/product-image2.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 2,
+//         name: 'Product 2',
+//         category: 'shoes',
+//         price: 20.99,
+//         image: '../images/product-image2.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 2,
+//         name: 'Product 2',
+//         category: 'shoes',
+//         price: 20.99,
+//         image: '../images/product-image2.jpg',
+//         details:'',
+//     },
+//     {
+//       id: 3,
+//       name: 'Product 3',
+//       category: 'hand-bags',
+//       price: 30.99,
+//       image: '../images/product-image11.jpg',
+//       details:'',
+//     },
+//     {
+//         id: 3,
+//         name: 'Product 3',
+//         category: 'hand-bags',
+//         price: 30.99,
+//         image: '../images/product-image11.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 3,
+//         name: 'Product 3',
+//         category: 'hand-bags',
+//         price: 30.99,
+//         image: '../images/product-image11.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 3,
+//         name: 'Product 3',
+//         category: 'hand-bags',
+//         price: 30.99,
+//         image: '../images/product-image11.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 3,
+//         name: 'Product 3',
+//         category: 'hand-bags',
+//         price: 30.99,
+//         image: '../images/product-image11.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 3,
+//         name: 'Product 3',
+//         category: 'hand-bags',
+//         price: 30.99,
+//         image: '../images/product-image11.jpg',
+//         details:'',
+//     },
+//     {
+//       id: 4,
+//       name: 'Product 4',
+//       category: 'freezers',
+//       price: 40.99,
+//       image: '../images/product-image10.jpg',
+//       details:'',
+//     },
+//     {
+//         id: 4,
+//         name: 'Product 4',
+//         category: 'freezers',
+//         price: 40.99,
+//         image: '../images/product-image10.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 4,
+//         name: 'Product 4',
+//         category: 'freezers',
+//         price: 40.99,
+//         image: '../images/product-image10.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 4,
+//         name: 'Product 4',
+//         category: 'freezers',
+//         price: 40.99,
+//         image: '../images/product-image10.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 4,
+//         name: 'Product 4',
+//         category: 'freezers',
+//         price: 40.99,
+//         image: '../images/product-image10.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 4,
+//         name: 'Product 4',
+//         category: 'freezers',
+//         price: 40.99,
+//         image: '../images/product-image10.jpg',
+//         details:'',
+//     },
+//     {
+//       id: 5,
+//       name: 'Product 5',
+//       category: 'earphones',
+//       price: 50.99,
+//       image: '../images/product-image4.jpg',
+//       details:'',
+//     },
+//     {
+//         id: 5,
+//         name: 'Product 5',
+//         category: 'earphones',
+//         price: 50.99,
+//         image: '../images/product-image4.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 5,
+//         name: 'Product 5',
+//         category: 'earphones',
+//         price: 50.99,
+//         image: '../images/product-image4.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 5,
+//         name: 'Product 5',
+//         category: 'earphones',
+//         price: 50.99,
+//         image: '../images/product-image4.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 5,
+//         name: 'Product 5',
+//         category: 'earphones',
+//         price: 50.99,
+//         image: '../images/product-image4.jpg',
+//         details:'',
+//     },
+//     {
+//         id: 5,
+//         name: 'Product 5',
+//         category: 'earphones',
+//         price: 50.99,
+//         image: '../images/product-image4.jpg',
+//         details:'',
+//     },
+//     {
+//       id: 6,
+//       name: 'Product 6',
+//       category: 'fragrances',
+//       price: 60.99,
+//       image: 'https://via.placeholder.com/300x200',
+//       details:'',
+//     },
+//     {
+//       id: 7,
+//       name: 'Product 7',
+//       category: 'phones-tablets',
+//       price: 70.99,
+//       image: 'https://via.placeholder.com/300x200',
+//       details:'',
+//     },
+//     {
+//       id: 8,
+//       name: 'Product 8',
+//       category: 'electronics',
+//       price: 80.99,
+//       image: 'https://via.placeholder.com/300x200',
+//       details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//     },
+//     {
+//         id: 8,
+//         name: 'Product 8',
+//         category: 'electronics',
+//         price: 80.99,
+//         image: 'https://via.placeholder.com/300x200',
+//         details:'',
+//         },
+//     {
+//       id: 9,
+//       name: 'Product 9',
+//       category: 'beds',
+//       price: 90.99,
+//       image: 'https://via.placeholder.com/300x200',
+//       details:'',
+//     },
+//     {
+//       id: 10,
+//       name: 'Product 10',
+//       category: 'security-cameras',
+//       price: 100.99,
+//       image: 'https://via.placeholder.com/300x200',
+//       details:'',
+//     },
+//   ];
+const productsContainer = document.querySelector('.products');
+  async function loadProductsData() {
+    try {
+      const response = await fetch('../Scripts/shop.json'); // Load the JSON file
+      const productsData = await response.json();    // Parse JSON data
+      return productsData;
+    } catch (error) {
+      console.error('Error loading products data:', error);
+      alert('Unable to load data: please try again later')
+    }
+  }
+  // Get elements from DOM
+  const categoriesBtns = document.querySelectorAll('.category-btn');
+ // const productsContainer = document.querySelector('.products');
+  const openCartBtn = document.querySelector('.view-cart-btn');
+  const cart = document.querySelector('.cart');
+  const cartList = document.querySelector('.cart-list');
+  const cartTotal = document.querySelector('.cart-total');
+  const checkoutBtn = document.querySelector('.checkout-btn');
+  let cartItems = [];
+
+  // Load products on page load
+  window.addEventListener('load', () => {
+    loadProducts('all');
+  });
+
+  // Load products by category
+  categoriesBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const category = e.target.dataset.category;
+      loadProducts(category);
+    });
+  });
+  // Load products data and display in products container
+ async function loadProducts(category) {
+    const productsData =await loadProductsData();
+    console.log(productsData);
+    const filteredProducts = productsData.filter(
+      (product) => product.category == category
+    );
+    
+    productsContainer.innerHTML = '';
+    if (category=='all') {
+        loadAllProducts();
+        return;
+    }
+   else{
+    filteredProducts.forEach((product) => {
+      const productEl = document.createElement('div');
+      productEl.classList.add('product');
+      productEl.innerHTML = `<img src="${product.image}" alt="${product.name}">         
+      <div class="product-info">           
+      <h3 class="product-name">${product.name}</h3>          
+       <p class="product-price">$${product.price.toFixed(2)}</p>           
+       <button class="add-to-cart-btn" data-id="${product.id}"><i class="fas fa-cart-plus"></i></button>        
+        </div>`;
+
+      productsContainer.appendChild(productEl);
+    });
+      // Add event listener to add to cart button
+      const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
+      addToCartBtns.forEach((btn) => {
+        btn.addEventListener('click', addToCart);
+      });
+   }
+}
+async function loadAllProducts() {
+    productsContainer.innerHTML = '';
+    
+    const productsData =await loadProductsData();
+    productsData.forEach((product) => {
+      const productEl = document.createElement('div');
+      productEl.classList.add('product');
+      productEl.innerHTML = `<img src="${product.image}" alt="${product.name}">         
+      <div class="product-info">
+      <p class="product-price">$${product.price.toFixed(2)}</p>            
+      <h3 class="product-name">${product.name}</h3>                    
+       <button class="add-to-cart-btn" data-id="${product.id}"><i class="fas fa-cart-plus"></i></button>        
+        </div>`;
+
+      productsContainer.appendChild(productEl);
+      });
+      // Add event listener to add to cart button
+      const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
+      addToCartBtns.forEach((btn) => {
+        btn.addEventListener('click', addToCart);
+      });
+}
+// Add product to cart
+async function addToCart(e) {
+const productsData =await loadProductsData();
+const productId = parseInt(e.target.dataset.id);
+const product = productsData.find((product) => product.id === productId);
+// Check if product is already in cart
+const cartItem = cartItems.find((item) => item.id === productId);
+if (cartItem) {
+  cartItem.quantity++;
+  updateCart();
+ // return;
+}
+else{
+   // Add product to cart
+cartItems.push({
+  id: product.id,
+  name: product.name,
+  price: product.price,
+  image: product.image,
+  quantity: 1,
+});
+updateCart();
+saveCart();
+}
+
+}
+
+// Update cart UI and save to local storage
+function updateCart() {
+cartList.innerHTML = '';
+let total = 0;
+cartItems.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<img src="${item.image}" alt = "${item.name}">
+      <div class="item-info">
+        <h4>${item.name}</h4>
+        <p>Price: $${item.price.toFixed(2)}</p>
+        <p>Quantity: ${item.quantity}</p>
+        <button class="remove-item-btn" data-id="${item.id}">Remove</button>
+      </div>
+      <p class="item-price">$${(item.price * item.quantity).toFixed(2)}</p>`;
+    cartList.appendChild(li);
+    total += item.price * item.quantity;
+  });
+  
+  cartTotal.innerText = total.toFixed(2);
+  
+  // Add event listener to remove item button
+  const removeItemBtns = document.querySelectorAll('.remove-item-btn');
+  removeItemBtns.forEach((btn) => {
+    btn.addEventListener('click', removeFromCart);
+  });
+}
+
+// Remove product from cart
+function removeFromCart(e) {
+const productId = parseInt(e.target.dataset.id);
+cartItems = cartItems.filter((item) => item.id !== productId);
+updateCart();
+saveCart();
+}
+
+// Save cart to local storage
+function saveCart() {
+localStorage.setItem('cart', JSON.stringify(cartItems));
+}
+
+// Load cart from local storage
+function loadCart() {
+const cartData = localStorage.getItem('cart');
+if (cartData) {
+cartItems = JSON.parse(cartData);
+updateCart();
+}
+}
+let isCartOpen= false;
+function adjustCartBTN_content(){
+   if (!isCartOpen) {
+      openCartBtn.innerHTML=`<i class="fas fa-times"></i>`
+   } else {
+    openCartBtn.innerHTML=`<i class="fas fa-shopping-cart"></i>`
+   }
+   isCartOpen=!isCartOpen;
+}
+// Show or hide cart
+openCartBtn.addEventListener('click', () => {
+ cart.classList.toggle('active');
+    adjustCartBTN_content();
+});
+
+// Checkout button
+checkoutBtn.addEventListener('click', () => {
+alert('Thank you for your purchase!');
+cartItems = [];
+updateCart();
+saveCart();
+});
+
+// Load cart from local storage on page load
+window.addEventListener('load', loadCart);
